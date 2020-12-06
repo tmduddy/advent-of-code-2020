dc685e356094c73d2b36aaaa4a97b3060de6eee7
tmduddy <tmd44@pitt.edu>
2020-12-06 09:46:57 -0500

    solve day1B

--- day1.py
+++ day1.py
@@ -10,6 +10,7 @@ clean_data = [int(num[0]) for num in data if int(num[0]) <= goal]
 
 def check_number_against_list(seed_number, data_list, goal=goal):
     for element in data_list:
+        # print(f'checking: {seed_number} + {element} = {seed_number+element}')
         if seed_number + element == goal:
             return element
     return "not found"
@@ -35,17 +36,24 @@ def part_b(clean_data):
     * Specifically, they need you to find the 3 entries that
     * sum to 2020 and then multiply those 3 numbers together.
     '''
-    for i in range(1, len(clean_data)):
-        base_number = clean_data[i-1]
-        next_number = clean_data[i]
-        seed_number = base_number + next_number
+    result = "not found"
+    outer_data_list = clean_data.copy()
+    for base_number in clean_data:
+        # print(f'init clean_data {outer_data_list}')
+        del outer_data_list[0]
+        # print(f'post_del clean_data {outer_data_list}')
+        # print(base_number)
+        for i in range(len(outer_data_list)):
+            next_number = outer_data_list[i]
 
-        data_list = clean_data[i+1:] # slice off seed number(s)
-
-        result = check_number_against_list(seed_number,data_list)
-        if result == "not found":
-            continue
-        break
+            inner_data_list = outer_data_list[i+1:] # slice off seed number
+            new_goal = goal-base_number
+            result = check_number_against_list(next_number,inner_data_list,goal=new_goal)
+            if result == "not found":
+                continue
+            break
+        if result != "not found":
+            break
     
     print(base_number, next_number, result)
     if result == "not found":    
